25f7884d1179ae527a18613c8645ca59cf2b3708
wcwcwc2004 <121157513+wcwcwc2004@users.noreply.github.com>
2025-06-07 16:24:38 +0800

    Create manyan_body.js

---
+++ manyan_body.js
@@ -0,0 +1,30 @@
+/*
+ 本地解锁猫眼
+
+# 脚本制作 @NobyDa_Chat @wangfei021325
+# 加密来源 @PayNe
+====================================
+[rewrite_local]
+# 修改响应体
+^https:\/\/yanchu\.maoyan\.com\/.* url script-response-body https://raw.githubusercontent.com/wcwcwc2004/openkey/refs/heads/main/manyan_body.js
+
+[mitm]
+hostname = yanchu.maoyan.com
+====================================
+*/
+
+let body = $response.body;
+let obj;
+
+try {
+  obj = JSON.parse(body);
+  // 检查并修改 attrMaps.serverTime
+  if (obj.attrMaps && obj.attrMaps.serverTime) {
+    obj.attrMaps.serverTime = 4070908800000;
+  }
+  body = JSON.stringify(obj);
+} catch (e) {
+  console.log("Error parsing JSON: " + e);
+}
+
+$done({ body });
